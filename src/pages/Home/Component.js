9bb77e5d705666f1e955a0ec453aaf294f3da038
linnnruoo <linruo2014@gmail.com>
2019-04-29 21:44:08 +0800

    modify home page component

--- src/pages/Home/Component.js
+++ src/pages/Home/Component.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Spinner from "../../components/Spinner";
+import GridContainer from "../../components/GridContainer";
+import GridItem from "../../components/GridItem";
+import LocationCard from "../../components/LocationCard";
 
-const HomeComponent = () => {
-  return <></>;
+const HomeComponent = ({ locations, loading }) => {
+  if (loading) return <Spinner />;
+
+  const renderLocations = () => {
+    return locations.map((location, index) => {
+      return (
+        <GridItem key={index}>
+          <LocationCard location={location} key={location.id} />
+        </GridItem>
+      );
+    });
+  };
+
+  return <GridContainer>{renderLocations()}</GridContainer>;
 };
 
 export default HomeComponent;
--- src/pages/Home/Container.js
+++ src/pages/Home/Container.js
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
+import config from "../../config/env.js";
+import { getLocationsByCompanyId } from "../../api";
+
+import HomeComponent from "./Component";
 
 class HomeContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      locations: []
+    };
+  }
+
+  componentDidMount = () => {
+    getLocationsByCompanyId(config.companyId).then(locations => {
+      this.setState({
+        locations: locations,
+        loading: false
+      });
+    });
+  };
+
   render() {
-    return <></>;
+    return (
+      <HomeComponent
+        locations={this.state.locations}
+        loading={this.state.loading}
+      />
+    );
   }
 }
 
